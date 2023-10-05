6ba405e0ab6dc322f16d1ed9412e46e3dc0884b2
Araik <iaraik99@gmail.com>
2023-10-05 10:19:42 +0300

    Добавил чтение чтение файла через stream

---
+++ src/handlers/fileManager.js
@@ -0,0 +1,48 @@
+import path from "path";
+import fs from "fs";
+import chalk from "chalk";
+import JSONStream from "JSONStream"
+import { __dirname } from "../helpers/utils.js";
+
+export async function saveData(data) {
+  const fileName = `${data.filename}.json`;
+
+  const baseDir = path.join(__dirname, "..", "..", "data");
+  fs.access(baseDir, function (err) {
+    if (err && err.code === "ENOENT") {
+      fs.mkdir(baseDir); //Create dir in case not found
+    }
+  });
+
+  const savePath = path.join(baseDir, fileName);
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(savePath, JSON.stringify(data, null, 4), (err) => {
+      if (err) {
+        return reject(err);
+      }
+
+      console.log(
+        chalk.blue("Файл успешно сохранен: ") + chalk.blue.bold(fileName) + "\n"
+      );
+
+      resolve();
+    });
+  });
+}
+
+
+export function streamToString(filePath) {
+  const stream = fs.createReadStream(filePath, { encoding: "utf8" });
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  })
+}
+
+export async function readFile(filePath) {
+  const str = await streamToString(filePath)
+  return JSON.parse(str)
+}
